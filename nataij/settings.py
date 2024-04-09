29c5aa6b2e22cb6d2815597a21a361daa1d9d763
iauth2024 <161607179+iauth2024@users.noreply.github.com>
2024-04-09 16:47:11 +0530

    Update settings.py

--- nataij/settings.py
+++ nataij/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2au_4jgf7*mxddvcblo(75=b(4ob7tw@i86m2*@!ifs_3k3owi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','127.0.0.1', 'localhost']
 
 # Application definition
 
